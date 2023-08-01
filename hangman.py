2dfe83aa5ff1f569381c855c09ca46c4e0607f2a
Garfas <mj.garfas@gmail.com>
2023-08-01 19:25:45 +0300

    .

---
+++ hangman.py
@@ -0,0 +1,13 @@
+
+
+import random
+from words import WORD_LIST
+
+#Nustatoma konstanta
+max_ateempts = 10
+
+def choose_random_words() -> str:
+    return random.choice(WORD_LIST)
+
+def
+    
---
+++ words.py
@@ -0,0 +1,5 @@
+from typing import List
+
+#An example list of words for the Hangman game
+#žodžių sąrašo pavyzdys žaidimui Pakaruoklis
+WORD_LIST = ["hangman", "python", "computer", "keyboard", "developer"]
