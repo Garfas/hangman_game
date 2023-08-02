a104579338833febcf247739532d8e2752d325a0
Garfas <mj.garfas@gmail.com>
2023-08-02 20:15:45 +0300

    .

--- hangman.py
+++ hangman.py
@@ -1,29 +1,21 @@
-<<<<<<< Updated upstream
-=======
 
 import requests
 import random
-# from words import WORD_LIST
 from bs4 import BeautifulSoup
 
 def get_random_words(num_words):
-    url = "https://www.randomword.com"
     response = requests.get(url)
     soup = BeautifulSoup(response.txt, "html.praser")
     words = soup.find_all("div", {"class": "section", "id": "random_word"})
     word_list = [word.get_text().strip().lower() for word in words]
     random_words = random.sample(word_list, num_words)
 
-
 #Nustatoma konstanta
-MAX_ATEEMPTS = 10
-
-# def choose_random_words():
-#     return random.choice(WORD_LIST)
+MAX_ATTEMPTS = 10
 
 def initialize_game():
-    word_to_guess = choose_random_words()
-    attempts_left = MAX_ATEEMPTS
+    word_to_guess = random.choice(get_random_words(1))
+    attempts_left = MAX_ATTEMPTS
     guessed_letters = set()
 
     return word_to_guess, attempts_left, guessed_letters
@@ -36,17 +28,17 @@ def update_word_representation(word, guessed_letters) -> str:
 
 def game_cycle():
     while True:
-        word_to_guess, attempts_left, guessed_letter = initialize_game()
+        word_to_guess, attempts_left, guessed_letters = initialize_game()
 
         while attempts_left > 0:
-            current_representation = update_word_representation(word_to_guess, guessed_letter)
+            current_representation = update_word_representation(word_to_guess, guessed_letters)
             print(f'Word: {current_representation}')
             print(f'Attempts left: {attempts_left}')
 
             guess = get_guess()
 
             if len(guess) == 1:
-                guessed_letter.add(guess)
+                guessed_letters.add(guess)
                 if guess in word_to_guess:
                     print("Correct guess!")
                 else:
@@ -73,5 +65,3 @@ if __name__ == "__main__":
     print("Welcome to Hangman!")
     game_cycle()
 
-
->>>>>>> Stashed changes
