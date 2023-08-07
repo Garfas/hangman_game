6f359ab11cafb603c5cb92d1d31b62a6db343b9d
Garfas <mj.garfas@gmail.com>
2023-08-07 20:49:41 +0300

    .

--- hangman.py
+++ hangman.py
@@ -46,23 +46,46 @@ class HangmanDatabase:
         self.create_tables()
 
     def create_tables(self):
-        with self.conn:(
-                '''
-                Create tables if not exists user(
-                id integer primary key,
-                name text not null,
+        with self.conn:
+            self.conn.execute(
+            '''
+            CREATE TABLES IF NOT EXISTS user(
+                id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL,
+                surname TEXT NOT NULL,
                 email text not null unique
-                )'''
+            )
+            '''
         )
         self.conn.execute(
-            '''Create tables if not exists games (
-            id integer primary key,
-            user_id integer not null,
-            guessed_letters taxt not null,
-            won Boolean not null,
-            foreign key (user_id) References user (id)
-            )'''
+            '''
+            CREATE TABLES IF NOT EXISTS games (
+            id INTEGER PRIMARY KEY,
+            user_id INTEGER NOT NULL,
+            word TEXT NOT NULL,
+            attempts INTEGER NOT NULL,
+            guessed_letters TEXT NOT NULL,
+            won BOOLEAN NOT NULL,
+            FOREING (user_id) REFERENCES user (id)
+            )
+            '''
         )
+
+    def register_user(self, name: str, surname: str, email: str) -> int:
+        with self.conn:
+            cursor = self.conn.execute("INSERT INTO user (name, surname, email) VALUE (?, ?, ?)", (name, surname, email))
+            return cursor.lastrowid
+        
+    def save_games(self, user_id: int, game: HangmanGame, won: bool):
+        with self.conn:
+            guessed_letter_str = ",".join(sorted(game.guessed_letters))
+            self.conn.execute(
+                '''
+                INSERT INTO games (user_id, word, attempts, guessed_letters, won)
+                VALUE (?, ?, ?, ?, ?)
+                ''',
+                (user_id, game.word_to_guess, game.attempts_left, guessed_letter_str, won),
+            )
     
     def get_user_statistic(self, user_id: int) -> dict:
         with self.conn:
@@ -71,6 +94,32 @@ class HangmanDatabase:
             )
             row = cursor.fetchone()
             return {"games_played": row[0], "games_won": row[1]}
+        
+
+def get_words_from_website() -> List[str]:
+    url = "https://random-words-api.vercel.app/"
+    try:
+        response = requests.get(url)
+        response = raise_for_status()
+        words = response.json()
+        return words
+    except requests.exceptions.RequestException as e:
+        logging.error("Error fetching data from the web site. Pleas check your internet connection.")
+        return[]
+    except Exception as e:
+        logging.error("An error ocurred while fetching data from the website.")
+
+def main():
+    print("Welcome to Hangman!")
+    word_list = get_words_from_website()
+    if not word_list:
+        print("Fai;ed to get words from the website. Exiting the game.")
+        return
+    
+    db = HangmanDatabase
+    name = input("Enter your name: ")
+    surname = input("Enter your surname: ")
+    email = input("Enter your email: ")
         
 
     # def game_cycle():
