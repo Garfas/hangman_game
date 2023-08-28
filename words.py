22f6136db7f86cc03b109308bf1e8cbc83fc60e6
Garfas <mj.garfas@gmail.com>
2023-08-28 18:54:45 +0300

    .

--- words.py
+++
@@ -1,5 +0,0 @@
-# from typing import List
-
-# #An example list of words for the Hangman game
-# #žodžių sąrašo pavyzdys žaidimui Pakaruoklis
-# WORD_LIST = ["hangman", "python", "computer", "keyboard", "developer"]
